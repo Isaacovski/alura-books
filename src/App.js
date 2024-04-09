b559dca7124287a8b3606bd0492c3d1f69970339
Isaac <codebiisaac@gmail.com>
2024-04-09 18:10:19 -0300

    organizando e sinplificando o codigo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import './App.css';
+import './App.css'
 import Logo from './componentes/Logo'
-import perfil from './img/perfil.svg'
-import sacola from './img/sacola.svg'
-
-const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
-const icones = [perfil,sacola]
+import OpcoesHeader from './componentes/OpcoesHeader';
+import IconesHeader from './componentes/iconesHeader/estilo';
 
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
-        <Logo></Logo> 
-        <ul className='opcoes'>
-          { textoOpcoes.map( (texto)=> (
-            <li className='opcao'><p>{texto}</p></li>
-          ) ) }
-        </ul>
-        <ul className='icones'>
-            { icones.map( (icone) => (
-              <li className='icone'><img src={icone}></img></li>
-            )) }
-        </ul>
+        <Logo/>
+        <OpcoesHeader/>
+         <IconesHeader/>
       </header>
     </div>
   );
