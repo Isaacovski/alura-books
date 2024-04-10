8d51621a5e3e00e1ff419b817e120982178f317c
Isaac <codebiisaac@gmail.com>
2024-04-09 21:03:50 -0300

    descancar pq o homi ne de ferro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css'
 import Header from './componentes/Header/index'
+import styled from 'styled-components/'
 
+const AppContainer = styled.div`
+  .App {
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg, #002F52 35%, #326589);
+  }
+  
+  li {
+    list-style: none;
+  }
+`
 
 function App() {
   return (
