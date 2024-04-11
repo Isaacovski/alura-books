d828204bc78452347503d957929a1a719fea0764
Isaac <codebiisaac@gmail.com>
2024-04-11 00:58:24 -0300

    criando componente de search

---
+++ src/componentes/Pesquisa/index.js
@@ -0,0 +1,12 @@
+function Pesquisa () {
+  return(
+     <section>
+        <h2>já sabe por onde começar ?</h2>
+        <h3>eNCONTRE SEU LIVRO EM NOSSA ESTANTE</h3>
+        <input placeholder="escreva sua proxima leitura" 
+        />
+      </section>
+  )
+}
+
+export default Pesquisa
