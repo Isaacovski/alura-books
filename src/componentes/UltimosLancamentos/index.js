c9e4563535f89306c0a036cefd3b9b988508d5a8
Isaac <codebiisaac@gmail.com>
2024-04-13 16:12:15 -0300

    deixando titulo dinamico

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,21 +1,12 @@
 import { livros } from './dadosUltimosLancamentos'
 import styled from 'styled-components'
+import {Titulo} from '../Tiyulo'
 
 const UltimosLancamentosContainer = styled.section`
     background-color: #EBECEE;
     padding-bottom: 20px;
     display: flex;
     flex-direction: column;
-`
-
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
 `
 
 const NovosLivrosContainer = styled.div`
@@ -29,7 +20,11 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
     return (
         <UltimosLancamentosContainer>
-            <Titulo>Ultímos Lançamentos</Titulo>
+            <Titulo cor="#EB9B00" 
+            tamanhoFonte='36px' 
+            alinhamento='center'>
+            Ultímos Lançamentos
+            </Titulo>
             <NovosLivrosContainer>
                 { livros.map( livro => (
                     <img src={livro.src}/>
