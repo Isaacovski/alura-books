596c49f32260fd06312c6d15f0b99a80faa91f32
Isaac <codebiisaac@gmail.com>
2024-04-13 02:49:02 -0300

    barra de pesquisa funcionando iniciando criação de novos componentes

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,6 +1,9 @@
+/* eslint-disable jsx-a11y/alt-text */
 import Input from '../Input'
 import styled from 'styled-components'
 import { useState } from 'react'
+import {livros} from './dadosPesquisa'
+
 
 const PesquisaContainer = styled.section`
   background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -23,18 +26,46 @@ const Subtitulo = styled.h3`
   font-weight: 500;
   margin-bottom: 40px;
 `
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+function Pesquisa() {
+  const [ livrosPesquisados, setLivrosPesquisados ] = useState([])
 
-function Pesquisa () {
-  const [textoDigitado,setTextoDigitado] = useState('')
-  return(
-     <PesquisaContainer>
-         <Titulo>já Sabe Por Onde Começar ?</Titulo>
-        <Subtitulo>Encontre Seu Livro Em Nossa Estante</Subtitulo>
-        <Input 
-        placeholder="escreva sua proxima leitura" 
-        onBlur={evento => setTextoDigitado(evento.target.value)}
-        />
-        <p>{textoDigitado}</p>
+  console.log(livrosPesquisados)
+
+  return (
+      <PesquisaContainer>
+          <Titulo>Já sabe por onde começar?</Titulo>
+          <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
+          <Input
+              placeholder="Escreva sua próxima leitura"
+              onBlur={evento => {
+                  const textoDigitado = evento.target.value
+                  const resultadoPesquisa = livros.filter
+                  ( livro => livro.nome.includes(textoDigitado) )
+                  setLivrosPesquisados(resultadoPesquisa)
+              }}
+          />
+            { livrosPesquisados.map( livro => (
+                <Resultado>
+                    <p>{livro.nome}</p>
+                    <img src={livro.src}></img>
+                </Resultado>
+            ) ) }
       </PesquisaContainer>
   )
 }
